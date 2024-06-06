c2d932a66aa1f84aad025e4fd3e4e29939c372de
ewislla <145395835+ewislla@users.noreply.github.com>
2024-06-06 19:54:21 +0100

    Create exercise2.c

---
+++ exercise2.c
@@ -0,0 +1,13 @@
+//NAME: Udhedhe Wisdom 
+//DEPARTMENT: Software Engineering 
+//MATRIC: CMP2307625
+#include <stdio.h>
+
+int main() {
+    //This Section loops from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
